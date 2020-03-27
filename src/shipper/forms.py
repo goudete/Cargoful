be47621e1eff0df403d1eb33f13fb5f9c45847c0
Rowan-W-Fitch <rfitch@oxy.edu>
2020-03-26 22:10:28 -0700

    added dropdown calendar and dropdown time

--- src/shipper/forms.py
+++ src/shipper/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from .models import order
 from crispy_forms.helper import FormHelper
-
+from datetime import datetime
+from django.contrib.admin.widgets import AdminDateWidget
 
 class Order_Form(forms.ModelForm):
     class Meta:
@@ -14,3 +15,7 @@ class Order_Form(forms.ModelForm):
             'contents',
             'instructions'
             ]
+        widgets = {
+            'pickup_date': forms.DateInput(attrs={'id':'datepicker'}),
+            'pickup_time': forms.TimeInput(attrs = {'id': 'timepicker'})
+        }
